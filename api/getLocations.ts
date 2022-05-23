baed1c77d73653005d09a1527df3b324d97d94f6
iknowmagic <ilan@iknowmagic.net>
2022-05-22 22:14:08 -0700

    vercel typings

--- api/getLocations.ts
+++ api/getLocations.ts
@@ -1,8 +1,10 @@
 import json from './data.json'
+import _orderBy from 'lodash/orderBy'
+import _keys from 'lodash/keys'
 import _groupBy from 'lodash/groupBy'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  const result = Object.keys(_groupBy(json, 'location').sort())
+  const result = _orderBy(_keys(_groupBy(json, 'location')))
   res.end(JSON.stringify(result))
 }
